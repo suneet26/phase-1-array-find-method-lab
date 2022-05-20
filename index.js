a01cf577868114293c306e528664327810024841
Sunee Thoedpraiphanawan <amythoedpranawan@yahoo.com>
2022-05-20 10:13:46 -0600

    commit tree 8cede85c49b6230d4328505731342e38d77ff8b2

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+function superbowlWin(record) {
+    let result = record.find( record => record.result === "W")
+    return !!result ? result.year : undefined;
+}
+
+
+// superbowlWin = (record) => {
+//     let result = record.find( record => record.result === "W")
+//     return !!result ? result.year : undefined;
+// }
